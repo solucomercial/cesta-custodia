873444e396cad7c46299b16e56762d32dd47db89
gui1416 <guilhermerm2005@gmail.com>
2026-03-05 09:14:11 -0300

    fix: ssl conection

--- api/src/lib/db.ts
+++ api/src/lib/db.ts
@@ -4,7 +4,9 @@ import * as schema from '@/lib/db/schema'
 import { databaseSslEnabled, databaseSslRejectUnauthorized, env } from '@/lib/env'
 
 const client = postgres(env.DATABASE_URL, {
-  ssl: require,
+  ssl: {
+    rejectUnauthorized: false,
+  },
   connect_timeout: 30,
   prepare: false,
 })
